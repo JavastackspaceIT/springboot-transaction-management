7d373d8d1b92416700f96c82e9ed55666942d6db
rakesh <rksinghyadav1995@gmail.com>
2020-12-28 16:25:31 +0530

    added comment in main method for bootstraping

--- src/main/java/com/javastackspaceit/FlightbookingTransactionExampleApplication.java
+++ src/main/java/com/javastackspaceit/FlightbookingTransactionExampleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 public class FlightbookingTransactionExampleApplication {
-
+//this is main method for bootstraping the springboot application 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightbookingTransactionExampleApplication.class, args);
 	}
